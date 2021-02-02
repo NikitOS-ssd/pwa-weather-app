5361ddc96b5ba156161d22e3b28efcee6dee969e
NikitOs <exemplum777@gmail.com>
2021-02-03 01:17:41 +0300

    Init project

---
+++ build/precache-manifest.03d5fe9bd1d6f23559f7956ada0365e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f66da8929ec5da914698d0265241f83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "653fc63702222927cd9e",
+    "url": "/static/css/main.1dea5049.chunk.css"
+  },
+  {
+    "revision": "4c3d82bc8c5d5ad9f7e8",
+    "url": "/static/js/2.5aa3f3db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5aa3f3db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653fc63702222927cd9e",
+    "url": "/static/js/main.619cd40c.chunk.js"
+  },
+  {
+    "revision": "64e1f3674d27e70267cb",
+    "url": "/static/js/runtime-main.d6e7beab.js"
+  },
+  {
+    "revision": "d2799c59652a8d322a4ba178196b93bd",
+    "url": "/static/media/app.d2799c59.jpg"
+  },
+  {
+    "revision": "e334268d379260f8c75fb0392cca6cd3",
+    "url": "/static/media/cold.e334268d.jpg"
+  },
+  {
+    "revision": "be277a1015d914867f0d5c8100bf96b7",
+    "url": "/static/media/hot.be277a10.jpg"
+  }
+]);
---
+++ src/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  API_KEY: '55372cd6962f4028b77160038201605',
+  BASE_URL: 'https://api.weatherapi.com/v1/current.json?key='
+}
